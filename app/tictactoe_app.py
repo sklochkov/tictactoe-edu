bf46377b8350f33ddf59d4a3be1d19f3cf7d48ef
Mamonova Ksenia <djuvia.loxar@yandex.ru>
2021-09-09 21:01:02 +0300

    added sign up function

--- app/tictactoe_app.py
+++ app/tictactoe_app.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
 import flask
+from uuid import uuid4 as uuid
+import re
+from random import choice
+
+SANITIZE_NICKNAME_RE = re.compile(r"[^A-Za-z0-9\-_=+\.]")
+
+DEFAULT_ADJ = ["Cool", "Funny", "Little", "Cute"]
+DEFAULT_NAME = ["Cat", "Dog", "Hamster", "Parrot"]
+
+
+def sanitize_nickname(username):
+    res = SANITIZE_NICKNAME_RE.sub('', username)
+    if not res:
+        return choice(DEFAULT_ADJ) + choice(DEFAULT_NAME)
+    return res
 
 class TictactoeApp(flask.Flask):
     def __init__(self, *args, **kwargs):
@@ -9,5 +24,16 @@ app = TictactoeApp(__name__)
 
 @app.route('/api/ping')
 def ping():
-    return "pong"
+    return 'pong'
+
+@app.route('/api/signup', methods=['POST'])
+def signUp():
+    username = sanitize_nickname(flask.request.form.get('username'))
+    
+    res = flask.make_response('', 302)
+    res.headers['Location'] = flask.request.environ['REQUEST_SCHEME'] + '://' + flask.request.environ['HTTP_HOST'] + '/GAME_CODE'
+    res.set_cookie(b'ID', bytes(str(uuid()), 'utf-8'))
+    res.set_cookie(b'username', bytes(username, 'utf-8'))
+    return res
+
 
